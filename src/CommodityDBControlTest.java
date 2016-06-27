9652ab691a0948be27dfdb606aaf4c4e05b7eb27
maleiber <choufengwudi@gmail.com>
2016-06-27 11:54:23 +0800

    160627-11:54

--- src/CommodityDBControlTest.java
+++ src/CommodityDBControlTest.java
@@ -97,18 +97,116 @@ public class CommodityDBControlTest {
 	}
 	
 	@Test
+	public void testGetNumInStock(){
+		int num=0;
+		
+		//when
+		num=cdbc.getNumInStock("TEST000000");
+		//then
+		if(num!=0)fail("get Commodity Stock wrong. it should be 0 ,but not.");
+		
+		//when
+		num=cdbc.getNumInStock("TEST000001");
+		//then
+		if(num!=1)fail("get Commodity Stock wrong. it should be 1 ,but not.");
+		
+		//when
+		num=cdbc.getNumInStock("TEST000002");
+		//then
+		if(num!=2)fail("get Commodity Stock wrong. it should be 2 ,but not.");
+		
+		//when
+		num=cdbc.getNumInStock(CommodityBarcode[3]);
+		if(num!=0)fail("get not exist Commodity Stock wrong. it should be 0 ,but not.");
+		//then
+	}
+	
+	@Test
 	public void testSetNum(){
-		ResultSet rs = null;
-		Statement statement = null;
+		
 		int stock_num;
 		
 		//when
-		
+		stock_num=cdbc.getNumInStock(CommodityBarcode[0]);
+		cdbc.setNum(CommodityBarcode[0], 1);
+	
 		//then
+		if(cdbc.getNumInStock(CommodityBarcode[0])!=1)
+			fail("set num of"+CommodityBarcode[0]+"wrong, it should be 1");
+		cdbc.setNum(CommodityBarcode[0], stock_num);
+		
+		
+	
+		//when
+		try{
+			cdbc.setNum(CommodityBarcode[3], 1);
+		//then	
+		}catch (SQLException e){
+			fail("DB error");
+		}catch (Exception e){
+			return;
+		}
+		fail("set num of not exist commodity error");
+		
+		
+	
 	}
 	
 	@Tset
 	public void testChangeCommodityNum(){
+		int oldStock=0,newStock=0;
+		
+		//when add num
+		oldStock=cdbc.getNumInStock(CommodityBarcode[0]);
+		newStock=oldStock-2;
+		cdbc.changeCommodityNum(CommodityBarcode[0], oldStock-newStock);
+		//then
+		if(cdbc.getNumInStock(CommodityBarcode[0])!=newStock)
+			fail("stock add wrong");
+		
+		//when sub num
+		oldStock=cdbc.getNumInStock(CommodityBarcode[1]);
+		newStock=oldStock+1;
+		cdbc.changeCommodityNum(CommodityBarcode[1], oldStock-newStock);
+		//then
+		if(cdbc.getNumInStock(CommodityBarcode[1])!=newStock)
+			fail("stock sub wrong");
+		
+		//when change 0
+		oldStock=cdbc.getNumInStock(CommodityBarcode[2]);
+		cdbc.changeCommodityNum(CommodityBarcode[2], 0);
+		//then
+		if(cdbc.getNumInStock(CommodityBarcode[2])!=oldStock)
+			fail("stock change 0 wrong");
+		
+		//when add exceed num
+		oldStock=cdbc.getNumInStock(CommodityBarcode[0]);
+		boolean status=false;
+		try{
+			cdbc.changeCommodityNum(CommodityBarcode[0], -99999);
+			//then
+		}catch (Exception e){
+			status=true;
+		}
+		if(!status)fail("stock add exceed num wrong");
+		
+		//when add not exist commodity
+		try{
+			oldStock=cdbc.getNumInStock(CommodityBarcode[3]);
+		}catch(Exception e){
+			fail("DB error when add not exist commodity");
+		}
+		
+		status=false;
+		try{
+			cdbc.changeCommodityNum(CommodityBarcode[3], 1);
+		}catch(Exception e){
+			if(!e.getMessage().equals("商品码错误！"))
+				fail("stock change error when change a not exist commodity but not wrong as 商品码错误！");
+			status=true;
+		}
+		if(!status)fail("stock change error when change a not exist commodity but succedded");
+		
 		
 	}
 	
